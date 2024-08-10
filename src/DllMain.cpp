9b6f8b550e44b1ededee2a10024be139471d26d2
Ruben Tytgat <ruben_tytgat@hotmail.com>
2024-08-10 23:35:56 +0200

    Rendering on 2 panes

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -238,7 +238,7 @@ class KeyEventHandler : public hh::fw::KeyEventHandler, public hh::game::GameMan
 					psi.playerId = 1;
 					psi.characterId = static_cast<app::player::CharacterIdU8>(keyEventArgs.scanCode - 0x22);
 					psi.worldPos.m_Position = levelInfo->GetPlayerInformation(0)->position.value;
-					psi.worldPos.m_Rotation = csl::math::Quaternion::Identity;
+					psi.worldPos.m_Rotation = csl::math::Quaternion::Identity();
 					psi.deadFallTime = fxParamMgr->sceneParameters[fxParamMgr->currentSceneParameters]->sceneData->stageConfig.common.deadFallTime;
 					psi.deadline = fxParamMgr->sceneParameters[fxParamMgr->currentSceneParameters]->sceneData->stageConfig.common.deadline;
 					psi.oceanSurface = fxParamMgr->sceneParameters[fxParamMgr->currentSceneParameters]->sceneData->stageConfig.common.oceanSurface;
@@ -248,6 +248,10 @@ class KeyEventHandler : public hh::fw::KeyEventHandler, public hh::game::GameMan
 				}
 			}
 		}
+		//if (keyEventArgs.scanCode == 0x26) {
+		//	auto* engine = static_cast<hh::gfx::RenderingEngineRangers*>(static_cast<hh::gfx::RenderManager*>(hh::gfx::RenderManager::GetInstance())->GetNeedleResourceDevice());
+		//	engine->mainRenderUnit->pipelineInfo->globalParameterBuilder->unk4.reserve(2);
+		//}
 		return false;
 	}
 };
@@ -266,11 +270,145 @@ HOOK(uint64_t, __fastcall, GameModeBootInit, 0x14734FB80, app::game::GameMode* s
 	return res;
 }
 
+class PaneRenderUnit : public hh::needle::RenderUnit {
+	unsigned int index;
+public:
+	PaneRenderUnit(const char* name, hh::needle::SupportFXAll* supportFX, uint8_t idx, unsigned int flags) : RenderUnit{ name, supportFX, static_cast<unsigned char>(0x80 + idx), flags }, index{ idx } {
+		cameraId = 0;
+		if (idx > 0)
+			createSceneParamListeners = false;
+	}
+
+	virtual bool IsEnabled(const RenderInfo& renderInfo) override {
+		return cameraId < renderInfo.renderParam.numViewports;
+	}
+
+	virtual void SetRenderDimensions(const RenderInfo& renderInfo) override {
+		//auto& params = renderingPipelineExecContext->globalParameters;
+
+		//float viewportInfo[4] = { cameraId == 0 ? 1.0f : 0.0f, cameraId == 1 ? 1.0f : 0.0f, cameraId == 2 ? 1.0f : 0.0f, cameraId == 3 ? 1.0f : 0.0f };
+		//params->SetShaderParameterFloatByName("u_current_viewport_mask", viewportInfo, 1);
+
+		//float screenInfo[4] = { 1280.0f, 720.0f, 0.0f, 0.001f };
+		//params->SetShaderParameterFloatByName("u_screen_info", screenInfo, 1);
+	}
+
+	virtual void LoadRenderParams(const RenderInfo& renderInfo) override {
+		RenderUnit::LoadRenderParams(renderInfo);
+		pipelineInfo->renderParam.viewports[0] = pipelineInfo->renderParam.viewports[index];
+	}
+};
+
+static hh::needle::intrusive_ptr<PaneRenderUnit> renderUnit2{};
+
+HOOK(void, __fastcall, RenderingEngineRangers_SetupMainRenderUnit, 0x14F2C0DF0, hh::gfx::RenderingEngineRangers* self) {
+	auto* staticAlloc = reinterpret_cast<csl::fnd::IAllocator*>(0x143CF5268);
+
+	auto* renderingPipeline = new hh::needle::RenderingPipelineRangers{ staticAlloc, self->supportFX.modelInstanceRenderer };
+	self->mainRenderingPipeline = renderingPipeline;
+
+	auto* renderUnit1 = new PaneRenderUnit{ "MainScene", &self->supportFX, 0, 0x2000u };
+	self->mainRenderUnit = renderUnit1;
+
+	renderUnit1->LoadGlobalParams();
+	renderUnit1->SetSceneName("MainScene");
+	renderUnit1->SetPipeline(renderingPipeline);
+
+	self->supportFX.AddRenderUnit(renderUnit1);
+
+	renderUnit2 = new PaneRenderUnit{ "MainScene2", &self->supportFX, 1, 0x2000u };
+	//self->mainRenderUnit = renderUnit2;
+
+	renderUnit2->LoadGlobalParams();
+	renderUnit2->SetSceneName("MainScene");
+	renderUnit2->SetPipeline(renderingPipeline);
+
+	self->supportFX.AddRenderUnit(renderUnit2);
+	self->supportFX.mainSceneName = "MainScene";
+}
+
+HOOK(void, __fastcall, CachedShadowMapRenderJob_Render, 0x141036290, hh::needle::CachedShadowMapRenderJob* self, hh::needle::PipelineInfo* pipelineInfo) {
+	if (self->viewportId < pipelineInfo->renderParam.numViewports && self->renderingPipeline)
+	{
+		auto renderTargetManager = pipelineInfo->supportFX->GetRenderManager()->renderTargetManager;
+		unsigned long long jobMask;
+
+		if (pipelineInfo->drawPassCount < 2) {
+			jobMask = 0x1F;
+		}
+		else if (pipelineInfo->currentPass > 0 && pipelineInfo->sceneParamContainer->GetSceneParam<hh::needle::FxShadowMapParameter>()->isSet) {
+			self->renderingPipeline->RenderMatchingJobs(pipelineInfo, (1ull << (pipelineInfo->currentPass - 1)) & 0xFull, true);
+			return;
+		}
+		else {
+			jobMask = 0x10ull;
+		}
+
+		if (self->unk119) {
+			for (unsigned int i = 10; i <= 15; ++i)
+				renderTargetManager->ClearDepthStencil({ i }, false);
+
+			self->unk119 = 0;
+		}
+
+		self->renderingPipeline->RenderMatchingJobs(pipelineInfo, jobMask, true);
+	}
+}
+
+//static hh::needle::SCullGroupSetting cullSettings[9 * 2]{};
+//
+//HOOK(void, __fastcall, WorldRenderingPipeline_Setup, 0x154C029B0, hh::needle::WorldRenderingPipeline* self, hh::needle::WorldRenderingPipeline::SetupInfo& setupInfo) {
+//	setupInfo.renderParameters.cullingGroupSettings = cullSettings;
+//	originalWorldRenderingPipeline_Setup(self, setupInfo);
+//}
+
 BOOL WINAPI DllMain(_In_ HINSTANCE hInstance, _In_ DWORD reason, _In_ LPVOID reserved)
 {
 	switch (reason)
 	{
 	case DLL_PROCESS_ATTACH:
+
+		//WRITE_MEMORY(0x146A5F3CE, unsigned int, 0x1C000000);
+		//// set rendermanager numMainViewports to 2
+		WRITE_MEMORY(0x14F382288, uint8_t, 2);
+
+		// load the same viewport settings for all viewports
+		WRITE_MEMORY(0x14F3822C9, uint8_t, 0x48, 0x89, 0xCA, 0x90);
+
+		WRITE_MEMORY(0x143CFB388, unsigned long, 0);
+
+		//WRITE_NOP(0x140D1732C, 5);
+		//WRITE_NOP(0x140D17337, 5);
+		//WRITE_NOP(0x140D160B1, 5);
+		////WRITE_NOP(0x140D1732C, 5);
+		////WRITE_NOP(0x140D1732C, 5);
+
+		//// set pipeline viewport count to 2
+		//WRITE_MEMORY(0x14FA98FE1, unsigned int, 2);
+
+
+		//WRITE_MEMORY(0x14FA990D1, unsigned int, 9);
+
+
+
+
+		// always get global params for viewport 0, even when rendering other viewports
+		//WRITE_MEMORY(0x14104E33F, uint8_t, 0x48, 0x8B, 0x00, 0x90);
+
+
+
+		// don't cap number of viewports in async jobs by global param count
+		//WRITE_MEMORY(0x141048EF4, uint8_t, 0x90, 0x90, 0x90);
+
+
+
+		//memcpy(cullSettings, reinterpret_cast<void*>(0x1415F9D80), sizeof(hh::needle::SCullGroupSetting) * 9);
+		//memcpy(cullSettings + 9, reinterpret_cast<void*>(0x1415F9D80), sizeof(hh::needle::SCullGroupSetting) * 9);
+
+		//INSTALL_HOOK(WorldRenderingPipeline_Setup);
+
+		INSTALL_HOOK(CachedShadowMapRenderJob_Render);
+		INSTALL_HOOK(RenderingEngineRangers_SetupMainRenderUnit);
 		INSTALL_HOOK(LevelInfo_GetPlayerObject);
 		INSTALL_HOOK(LevelInfo_GetPlayerInformation);
 		INSTALL_HOOK(LevelInfo_GetPlayerIdFromPlayerObject);
